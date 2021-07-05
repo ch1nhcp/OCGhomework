1ffa83da2b09e55151d97335a6268176585d4db1
ch1nhcp <chinhcpdev@gmail.com>
2021-07-05 17:24:50 +0700

    update

---
+++ Lecture2/1-API/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Please add params!")
+	fmt.Println("This is homepage")
+}
+
+func expression(w http.ResponseWriter, r *http.Request) {
+	exp := r.URL.Query().Get("exp")
+
+	params1 := r.URL.Query().Get("num1")
+	fmt.Println(params1)
+	params2 := r.URL.Query().Get("num2")
+	num1, _ := strconv.Atoi(params1)
+	fmt.Println(num1)
+	num2, _ := strconv.Atoi(params2)
+
+	switch exp {
+	case "add":
+		result := num1 + num2
+		fmt.Fprintf(w, "result = %d", result)
+
+	case "sub":
+		result := num1 - num2
+		fmt.Fprintf(w, "result = %d", result)
+
+	case "mul":
+		result := num1 * num2
+		fmt.Fprintf(w, "result = %d", result)
+
+	case "div":
+		result := num1 / num2
+		fmt.Fprintf(w, "result = %d", result)
+
+	}
+
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/exp", expression)
+	log.Fatal(http.ListenAndServe(":2800", nil))
+}
+
+func main() {
+	handleRequests()
+}
