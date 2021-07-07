b7464390fc95ea3d5cb5052ccfd32678a8a3aa10
ch1nhcp <chinhcpdev@gmail.com>
2021-07-07 07:41:37 +0700

    update

--- Lecture2/4-primenumber/main.go
+++ Lecture2/4-primenumber/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -32,15 +33,15 @@ func PrimeNumber(a []int) {
 		}
 	}
 	if error != 0 {
-		fmt.Println("It is Prime array!")
+		fmt.Println("It is not Prime array!")
 
 	} else {
-		fmt.Println("It is not Prime array!")
+		fmt.Println("It is Prime array!")
 	}
 }
 
 func readFile(file string) []int {
-	content, err := ioutil.ReadFile("textfile.txt")
+	content, err := ioutil.ReadFile(file)
 
 	if err != nil {
 		log.Fatal(err)
@@ -56,6 +57,36 @@ func readFile(file string) []int {
 	return newarray
 }
 
+
+func WriteFileByIO() {
+	//Bước 1: tạo file
+	file, err := os.Create("writed.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Bước 2: ghi file
+	message := []byte("Hi there!")
+	err = ioutil.WriteFile("writed.txt", message, 0644)
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	//Bước 3: đóng fileText
+	err = file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
 func main() {
 
+	var textFile []int
+	textFile = readFile("textfile.txt")
+	PrimeNumber(textFile)
 }
